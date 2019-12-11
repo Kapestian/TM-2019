1b76bd2228e3b3bef7483124e67d327432635fda
Valentin Porchet <46000121+Kapestian@users.noreply.github.com>
2019-12-11 17:00:53 +0100

    Revert "Revert "Revert "---"""
    This reverts commit c611a0b5338995ba83d24d327e9abc6a339b4da9.

--- hacker.py
+++ hacker.py
@@ -1,9 +1,7 @@
 import pygame
 from pygame.locals import *
 import os
-import pickle
 import time
-import terminal
 
 BLACK = (0, 0, 0)
 GRAY = (127, 127, 127)
@@ -46,6 +44,7 @@ class App:
         decryptor_icon.movable = False
         App.email_win = Icon(self, 'windows2/inbox_win.png', pos=(x, y)); y += dy
         Rectangle(self, Rect(0, 660, 1920, 65))
+        Terminal(self, 'Terminal', '> mkdir hacking_files', Rect(400,350, 500, 200))
         quit_button = Button(self, "button/shutdown.png", pos=(10, 665), cmd='App.running = False')
         
     def run(self):
@@ -242,248 +241,72 @@ class Rectangle(Node):
 
 class Window(Node):
     """Create a window object."""
-    def __init__(self, parent, image):
+    def __init__(self, parent, title, rect=Rect(100, 100, 300, 200)):
         super().__init__(parent, rect)
 
-        self.img = pygme.image.load(image)
+        self.title = title
+        self.rect = rect
+        self.border_color = BLACK
+        self.border_width = 3
+        self.background_color = WHITE
+        self.titlebar_color = DARKBLUE
         self.outlined = False
-
-        self.root = os.getcwd()
-        self.cwd = '' #curent working directory
-        self.cwd_level = 0 # +1 for every further directories
-        self.subdirs = None #avaible child directories
-        self.lsfiles = None #avaible files
-        self.game_lvl = folder #curent level
-        self.deletable_files = ['file1.txt']
-        self.deleted_files = []
-        self.display = []
-        self.prev_display = []
-        self.next_display = []
-        self.helpcmd =[
-            'ls\t Display a list of a directory\'s files and subdirectories',
-            'cd\t Change the current directory (ex: cd folder)',
-            'cd..\t Go to the parent directory',
-            'open \t Open specified file (ex: open file.txt)' ,
-            'access\t Connect to another device (ex: access device,password)',
-            'del\t Delete specified file (ex: del file.txt)',
-            'clue\t Show level\'s clue' ,
-            'answer\t Show level\'s answer',]
-
-        # ces données seront "encryptées" avec un pickle
-        self.clues = {'level1':'this is a clue', 'level2': 'this is another clue'}
-        self.answers = {'level1':'the password is "pswrd"', 'level2': 'this is another answer'}
-        self.devices = {'172.685': ('pswrd', 'level2')}
-
-        os.chdir(folder)
-        print(f'Terminal starting folder <{folder}>')
-        self.init_dir()  
+        
+        Text(self, title, fontcolor=WHITE, pos=(10, 10), movable=False, selectable=False, outlined=False)
 
     def draw(self, pos=(0, 0)):
         """Draw window with title bar."""
         super().draw(pos)
-
-        App.screen.blit(self.img,(90, 90))
+        pygame.draw.rect(App.screen, self.background_color, self.rect, 0)
+        pygame.draw.rect(App.screen, self.titlebar_color, (*self.rect.topleft, self.rect.width, 40))
+        pygame.draw.rect(App.screen, self.border_color, self.rect, self.border_width)
         for child in self.children:
             child.draw(self.rect.topleft)
-    
-    def init_dir(self):
-        dir_data = []
-        '''inits the directory datas'''
 
-        with open('folder_init','rb') as data:
-            dir_data = []
-            data_recover = pickle.Unpickler(data)
-            dir_data = data_recover.load()
+class Image(Window):
+    """Display an image in a window."""
+    def __init__(self, parent, file, pos=(100, 100)):
+        super().__init__(parent, file, pos)
 
-        self.cwd = dir_data[0]
-        self.subdirs = dir_data[1]
-        self.lsfiles = dir_data[2]
-        self.display_print(self.cwd, False)
-        print(f'{self.cwd}, sub-directories: {self.subdirs}, sub-files: {self.lsfiles}')
+        self.title = file
+        self.titlebar_color = DARKRED
+        self.img = pygame.image.load(file)
+        # self.img = pygame.transform.scale(self.img, (80, 80))
+        self.rect = self.img.get_rect()
+        self.rect.topleft = pos
 
-    def simulate(self):
-        while True:
-            line = input('>> ')
-            self.execute_cmd(line)
-
-
-    def execute_cmd(self, cmd_str): 
-        ''' read à terminal line (path:\> action argument) and execute an action'''
-
-        cmd_lst = cmd_str.split(' ')
-        action = cmd_lst[0]
-        try:
-            arg = cmd_lst[1]
-        except:
-            arg = None
-
-        if action == 'cd':
-            self.change_dir(arg)
-        elif action == 'cd..':
-            self.previous_dir()
-        elif action == 'ls':
-            self.list_files()
-        elif action == 'open':
-            self.open_file(arg)
-        elif action == 'access':
-            arg2 = arg.split(',')
-            target = arg2[0]
-            password = arg2[1]
-            self.access(target, password)
-        elif action == 'del':
-            self.del_file(arg)
-        elif action == 'clue':
-            self.get_clue()
-        elif action == 'answer':
-            self.get_answer()
-        elif action == 'cwd':
-            self.get_cwd() #debug only
-        elif action == 'help':
-            self.help_cmd()
-        else:
-            self.display_print('unable to execute this command')
-            print('unable to execute this command')
-
-    def display_print(self, text='', newline=True):
-        if len(self.display) < 5: #not full window
-            self.display.append(text)
-            if newline:
-                self.display.append(self.cwd)
-
-        else: #full window
-            stored = self.display.pop(0)
-            self.display.append(text)
-            self.prev_display.append(stored)
-            if newline:
-                stored = self.display.pop(0)
-                self.display.append(self.cwd + ' ')
-                self.prev_display.append(stored)
-    
-    def change_dir(self, newdir): #ajouter ligne curdir
-        if self.subdirs != None and newdir in self.subdirs:
-            self.cwd_level += 1
-            os.chdir(newdir)
-            self.init_dir()
-        else:
-            self.display_print('directory does not exist')
-            print('directory does not exist')
-
-    def previous_dir(self): #ajouter ligne curdir
-        if self.cwd_level > 0:
-            self.cwd_level -= 1
-            os.chdir('..')
-            self.init_dir()
-
-    def get_cwd(self): #debug only
-        print(self.cwd)
-
-    def list_files(self): #blit chaque ligne
-        if self.lsfiles != None:
-            for directory in self.subdirs:
-                dir_str = '<dir>\t{}'.format(directory)
-                self.display_print(dir_str)
-                print('<dir>', directory, sep='\t')
-
-            for file in self.lsfiles:
-                if file not in self.deleted_files:
-                    file_str = '<file>\t{}'.format(file)
-                    self.display_print(file_str)
-                    print('<file>', file, sep='\t')
-
-        else:
-            self.display_print('This folder is empty')
-            print('This folder is empty')
-
-    def open_file(self, file):
-        elt_file = file.split('.')
-        name = elt_file[0]
-        #extension = elt_file[1]
-
-        if file in self.lsfiles and file not in self.deleted_files:
-            #reading file datas
-            with open(name, 'rb') as openedfile:
-                data_recover = pickle.Unpickler(openedfile)
-                file_data = data_recover.load()
-            datatype = file_data[0]
-            content = file_data[1]
-            encrypted = file_data[2]
-
-            #display file if not encrypted
-            if encrypted:
-                self.display_print('this file is encrypted')
-                print('this file is encrypted')
-
-            elif datatype == 'txtfile':
-                self.display_print(f'text: {content}')
-                print(f'text: {content}')
-
-            elif datatype == 'pngfile':
-                self.display_print(f'image: {content}') # à modifier
-                print(f'image: {content}') # à modifier
-
-            elif datatype == 'msgfile':
-                self.display_print(f'e-mail: {content}')# à modifier
-                print(f'e-mail: {content}')# à modifier
-
-        else:
-            self.display_print('file does not exist')
-            print('file does not exist')
+    def draw(self, pos=(0, 0)):
+        super().draw(pos)
+        App.screen.blit(self.img, self.rect.move(pos).move(0, 40))
         
-    def access(self, target,password):
-        if target in self.devices.keys():
-            if self.devices[target][0] == password:
-                os.chdir(self.root)
-                terminal2 = Terminal(self.devices[target][1])
-                terminal2.simulate()
-            else:
-                self.display_print('wrong password')
-                print('wrong password')
-        else:
-            self.display_print('target device not found')
-            print('target device not found')
+class TextFile(Window):
+    """Create a folder object."""
+    def __init__(self, parent, name, lines, rect):
+        super().__init__(parent, name, rect)
 
-    def del_file(self, file):
-        if file in self.lsfiles:
-            if file in self.deletable_files:
-                self.deleted_files.append(file)
-            else:
-                self.display_print('unable to delete this file')
-                print('unable to delete this file')
-        else:
-            self.display_print('file does not exist')
-            print('file does not exist')
+        x, y = 10, 50
+        for line in lines.splitlines():
+            Text(self, line, pos=(x, y), movable=False, outlined=False)
+            y += 30
 
-    def get_clue(self):
-        self.display_print(self.clues[self.game_lvl])
-        print(self.clues[self.game_lvl])
+    def draw(self, img, pos=(0, 0)):
+        """Draw window."""
+        super().draw(pos)
+        
+        for child in self.children:
+            child.draw(self.rect.topleft)
 
-    def get_answer(self):
-        self.display_print(self.answers[self.game_lvl])
-        print(self.answers[self.game_lvl])
+class Terminal(Window):
+    """Create a terminal object."""
+    def __init__(self, parent, name, lines, rect):
+        super().__init__(parent, name, rect)
+        self.background_color = BLACK
+        self.titlebar_color = DARKGREEN
 
-    def clear_screen(self):
-        self.display, self.prev_display, self.next_display = [],[],[]
-        self.display_print(self.cwd)
-        print(self.cwd)
-
-    def help_cmd(self):
-        for line in self.helpcmd:
-            self.display_print(line, False)
-            print(line)
-        self.simulate()
-    def history_up(self):
-        if len(self.display) == 5 and len(self.prev_display) != 0:
-            x = self.prev_display.pop(-1)
-            y = self.display.pop(-1)
-            self.display.insert(0,x)
-            self.next_display.insert(0,y)
-
-    def history_down(self):
-        if len(display) == 5 and len(next_display) != 0:
-            x = self.display.pop(0)
-            y = self.next_display.pop(0)
-            self.prev_display.append(x)
-            self.display.append(y)
+        x, y = 10, 50
+        for line in lines.splitlines():
+            Text(self, line, pos=(x, y), movable=False, outlined=False, fontcolor=WHITE)
+            y += 30
 
 if __name__ == '__main__':
     App().run()
