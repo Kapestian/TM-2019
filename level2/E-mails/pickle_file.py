f03cd16e7c37a4f87d67b55081f46bf38a6eed97
Valentin Porchet <46000121+Kapestian@users.noreply.github.com>
2019-12-18 00:04:36 +0100

    --

---
+++ level2/E-mails/pickle_file.py
@@ -0,0 +1,11 @@
+import pickle
+
+def init_construct(file_name, path, encrypted=False):
+    data = []
+    data.append(path)
+    data.append(encrypted)
+    with open (file_name[:-4],'wb') as datafile:
+        pick = pickle.Pickler(datafile)
+        pick.dump(data)
+
+init_construct('ldc.txt','ldc.png')
--- level2/pickle_file.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def init_construct(file_name, path, encrypted):
-    data = []
-    data.append(path)
-    data.append(encrypted)
-    with open (file_name,'wb') as datafile:
-        pick = pickle.Pickler(datafile)
-        pick.dump(data)
